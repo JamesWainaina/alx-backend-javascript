ddb9a7b14926be56aca1178662ac907f9f920d62
JamesWainaina <jamesgatheru9905@gmail.com>
2024-06-03 15:26:54 +0300

    eslint fix

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,9 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    task = false;
-    task2 = true;
+    // Do not redeclare task and task2, but if necessary, modify the return or implementation logic here
   }
 
   return [task, task2];

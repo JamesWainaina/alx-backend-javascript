1a462a6d0166d803355ac42ac0ccbbecf6153e9a
JamesWainaina <jamesgatheru9905@gmail.com>
2024-06-05 20:14:20 +0300

    correction use of dataview to access buffers

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -3,10 +3,10 @@ function createInt8TypedArray(length, position, value) {
     throw new Error('Position outside range');
   }
   const buffer = new ArrayBuffer(length);
-  const view = new Int8Array(buffer);
-  view[position] = value;
+  const view = new DataView(buffer);
+  view.setInt8(position, value);
 
-  return buffer;
+  return view;
 }
 
 export default createInt8TypedArray;

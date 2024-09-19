89ee76cfcc52f41cdd6aedbd0d99d838f57a1389
JamesWainaina <JamesWainaina>
2024-09-19 17:09:32 +0300

    task 3 implement sinon test case

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,25 @@
+const calculateNumber = require("./2-calcul_chai");
+
+const Utils = {
+  calculateNumber: function calculateNumber(type, a, b) {
+    const roundedA = Math.round(a);
+    const roundedB = Math.round(b);
+
+    if (type === "SUM") {
+      return roundedA + roundedB;
+    }
+
+    if (type === "SUBTRACT") {
+      return roundedA - roundedB;
+    }
+
+    if (type === "DIVIDE") {
+      if (roundedB === 0) {
+        return "Error";
+      }
+      return roundedA / roundedB;
+    }
+  },
+};
+
+module.exports = Utils;
